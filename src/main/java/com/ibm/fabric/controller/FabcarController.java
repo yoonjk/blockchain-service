bd0b00f059e2edcb0fc0dc5e4994fcf70edcdfb7
yoonjk <nexweb1@gmail.com>
2021-07-04 23:54:00 +0900

    Add test

--- src/main/java/com/ibm/fabric/controller/FabcarController.java
+++ src/main/java/com/ibm/fabric/controller/FabcarController.java
@@ -26,6 +26,7 @@ public class FabcarController {
 		return "Hello";
 	}
 	
+	@ApiOperation(value = "cars", nickname = "cars search")
 	@GetMapping("/cars")
 	public String getAllCars() {
 		return fabcarService.getAllCars();
